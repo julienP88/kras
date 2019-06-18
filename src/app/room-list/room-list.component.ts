0a2aa4db8f95e7e65f91945fa513cf016c715b71
Anna Utlik <a.utlik@thalia.de>
2019-06-18 14:56:43 +0200

    icons

--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DataService} from '../data.service';
 import {Room} from "../shared/models/Room";
+import {AusstattungType} from "../shared/models/AusstattungType";
 import {Router} from "@angular/router";
 
 @Component({
@@ -23,7 +24,7 @@ export class RoomListComponent implements OnInit {
             this.roomsByFloor = new Map<String, Array<Room>>();
             response.forEach(room => {
                 var key = room.meta.etage;
-                if (!this.roomsByFloor.has(key)){
+                if (!this.roomsByFloor.has(key)) {
                     this.roomsByFloor.set(key, new Array<Room>());
                 }
                 this.roomsByFloor.get(key).push(room);
@@ -34,5 +35,28 @@ export class RoomListComponent implements OnInit {
 
     public showRoomDetails(room: Room) {
         this.router.navigate(['room-list', room.id]);
+    }
+
+    public getIconClass(icon: AusstattungType): string {
+        switch (icon) {
+            case AusstattungType.typ_appleTv:
+                return "fa fa-apple";
+            case AusstattungType.typ_pinnwand:
+                return "fa fa-apple";
+            case AusstattungType.typ_mobVK:
+                return "fa fa-apple";
+            case AusstattungType.typ_camera:
+                return "fa fa-apple";
+            case AusstattungType.typ_mobPolycom:
+                return "fa fa-apple";
+            case AusstattungType.typ_flipchart:
+                return "fa fa-apple";
+            case AusstattungType.typ_whiteboard:
+                return "fa fa-apple";
+            case AusstattungType.typ_tv:
+                return "fa fa-apple";
+            default:
+                return "fa fa-camera";
+        }
     }
 }
